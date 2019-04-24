c459f362a999752493f5fef495c630c3973901ff
MohamedElmdary <engm5081@gmail.com>
2019-04-24 20:50:51 +0200

    added bluetooth library

--- app/src/main/java/com/example/carcontroller/MainActivity.java
+++ app/src/main/java/com/example/carcontroller/MainActivity.java
@@ -7,8 +7,14 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
+import io.palaima.smoothbluetooth.Device;
+import io.palaima.smoothbluetooth.SmoothBluetooth;
+
 public class MainActivity extends AppCompatActivity {
 
+    private SmoothBluetooth mSmoothBluetooth;
     private BtnMoveType[] btnsMoveType = new BtnMoveType[4];
     private class BtnMoveType {
         public Button btn;
@@ -31,7 +37,84 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 4; i++) {
             addListenerToButtons(btnsMoveType[i]);
         }
+        mSmoothBluetooth = new SmoothBluetooth(MainActivity.this);
+        /*SmoothBluetooth.Listener mListener = */new SmoothBluetooth.Listener() {
+            @Override
+            public void onBluetoothNotSupported() {
+                //device does not support bluetooth
+                Log.d("onBluetoothNotSupported: ", "bluetooth not supported yet!");
+            }
+
+            @Override
+            public void onBluetoothNotEnabled() {
+                //bluetooth is disabled, probably call Intent request to enable bluetooth
+                Log.d("onBluetoothNotEnabled: ", "bluetooth not enabled yet!");
+            }
+
+            @Override
+            public void onConnecting(Device device) {
+                //called when connecting to particular device
+                Log.d("onConnecting: ", "bluetooth is connecting!");
+            }
+
+            @Override
+            public void onConnected(Device device) {
+                //called when connected to particular device
+                Log.d("onConnected: ", "connected and ready to use");
+                // here start listen to buttons clicks
+            }
+
+            @Override
+            public void onDisconnected() {
+                //called when disconnected from device
+                Log.d("onDisconnected: ", "bluetooth disconnected for some reason!");
+            }
+
+            @Override
+            public void onConnectionFailed(Device device) {
+                //called when connection failed to particular device
+                Log.d("onConnectionFailed: ", "bluetooth failed to connect for some reason!");
+            }
+
+            @Override
+            public void onDiscoveryStarted() {
+                //called when discovery is started
+                Log.d("onDiscoveryStarted: ", "starting to discover devices!");
+            }
+
+            @Override
+            public void onDiscoveryFinished() {
+                //called when discovery is finished
+                Log.d("onDiscoveryFinished: ", "discovering finished!");
+            }
+
+            @Override
+            public void onNoDevicesFound() {
+                //called when no devices found
+                Log.d("onNoDevicesFound: ", "no device was found");
+            }
+
+            @Override
+            public void onDevicesFound(final List<Device> deviceList,
+                                       final SmoothBluetooth.ConnectionCallback connectionCallback) {
+                //receives discovered devices list and connection callback
+                //you can filter devices list and connect to specific one
+                //connectionCallback.connectTo(deviceList.get(position));
+                Log.d("Devices number: ", "" + deviceList.size());
+            }
+
+            @Override
+            public void onDataReceived(int data ) {
+                //receives all bytes
+                // useless
+                Log.d("onDataReceived: ", "" + data);
+            }
+        };
+
+        mSmoothBluetooth.doDiscovery();
+
     }
+
 
     private void addListenerToButtons(final BtnMoveType clickedButton) {
         clickedButton.btn.setOnTouchListener(new View.OnTouchListener() {
@@ -43,9 +126,11 @@ public class MainActivity extends AppCompatActivity {
                     case MotionEvent.ACTION_MOVE:
                         // bluetooth sends the char which indicates the action to the car
                         Log.d("ACTION Type: ", clickedButton.type);
+                        mSmoothBluetooth.send(clickedButton.type);
                         break;
                     case MotionEvent.ACTION_UP:
                         Log.d("ACTION Type: ", "s");
+                        mSmoothBluetooth.send("s");
                         break;
                 }
                 return false;
